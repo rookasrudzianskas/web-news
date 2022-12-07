c12fd4b69388fdc06115522ea9b8de84bbd9e1b4
Rokas <rookas.rudzenskas@gmail.com>
2022-12-07 21:15:59 +0200

    feat: rename DataEntry to Article
    This is a breaking change.

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,7 +1,52 @@
+import {gql} from "graphql-request";
+
 export const fetchNews = async (category?: Category | string, keywords?: string, isDynamic?: boolean) => {
     // GraphQL query
-
+    const query = gql`
+        query MyQuery(
+            $access_key: String!
+            $categories: String!
+            $keywords: String
+        )   {
+            myQuery(
+                access_key: $access_key
+                categories: $categories
+                countries: "gb",
+                sort: "published_desc",
+                keywords: $keywords
+            ) {
+                data {
+                    author
+                    category
+                    country
+                    description
+                    image
+                    language
+                    published_at
+                    source
+                    title
+                    url
+                }
+                pagination {
+                    count
+                    limit
+                    offset
+                    total
+                }
+            }
+        }
+    `;
     // Fetch data from API
+
+    const res = await fetch('https://saltillo.stepzen.net/api/pruning-meerkat/__graphql', {
+        method: 'POST',
+        cache: isDynamic  ? 'no-cache' : 'default',
+        next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`
+        }
+    })
 }
 
 
