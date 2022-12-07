1123acd3edb645c2479b4d0ea122d2f7c34648fe
Rokas <rookas.rudzenskas@gmail.com>
2022-12-07 19:48:56 +0200

    Add stepzen.config.json
    This file is used by stepzen to generate the GraphQL schema.

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -3,3 +3,6 @@ export const fetchNews = async (category?: Category | string, keywords?: string,
 
     // Fetch data from API
 }
+
+
+// stepzen import curl http://api.mediastack.com/v1/news?access_key=c6c7156b60404163b8e625d0c8dc455d&sources=business,sports
